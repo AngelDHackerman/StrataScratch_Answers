0d6d6864bd62e14c61ac37e372115f0669a22a3a
Angel Hernandez <angeldariaux@gmail.com>
2025-10-19 11:36:04 -0600

    solved salary_differences

---
+++ SQL/salary_differences.sql
@@ -0,0 +1,14 @@
+SELECT 
+  (SELECT MAX(salary) 
+   FROM db_employee 
+   WHERE department_id = 1) AS max_engineering_salary,
+  
+  (SELECT MAX(salary) 
+   FROM db_employee 
+   WHERE department_id = 4) AS max_marketing_salary,
+  
+  (
+    (SELECT MAX(salary) FROM db_employee WHERE department_id = 4)
+    -
+    (SELECT MAX(salary) FROM db_employee WHERE department_id = 1)
+  ) AS salary_difference;
